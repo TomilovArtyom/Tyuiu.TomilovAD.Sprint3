430fa8ad8163668d13a94321043012636f85db57
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-11-01 00:30:01 +0500

    Task 4

---
+++ Tyuiu.TomilovAD.Sprint3.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task4.V14.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint3.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = -5;
+            int y = 5;
+            double wait = 6.263;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint3.Task4.V14/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task4.V14.Lib;
+
+DataService ds = new DataService();
+int x = -5;
+int y = 5;
+
+Console.WriteLine(ds.Calculate(x, y));
