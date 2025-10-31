dde0b66648b6cbd7b9fb7d917d9ccc1ee034e212
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-31 23:57:37 +0500

    Task 3

---
+++ Tyuiu.TomilovAD.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task3.V21.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            char c = 'e';
+            string x = "f3g5ht g4j 34kg4";
+            string wait = "fegeht gej eekge";
+            string res = ds.ReplaceNumOnChar(x, c);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task3.V21.Lib;
+
+DataService ds = new DataService();
+char c = 'e';
+string x = "f3g5ht g4j 34kg4";
+string wait = "fegeht gej eekge";
+Console.WriteLine(ds.ReplaceNumOnChar(x, c));
