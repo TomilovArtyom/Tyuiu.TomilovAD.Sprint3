a0371690ba6a9b9b3758b1999ca0a73e0836ee94
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-31 21:46:26 +0500

    Add project files.

---
+++ Tyuiu.TomilovAD.Sprint3.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task0.V1.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint3.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startvalue = 1;
+            int endvalue = 15;
+
+            double res = ds.GetSumSeries(startvalue, endvalue);
+
+            double wait = 375;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint3.Task0.V1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.TomilovAD.Sprint3.Task0.V1.Lib;
+
+DataService ds = new DataService();
+
+int startvalue = 1;
+int endvalue = 15;
+
+Console.WriteLine(ds.GetSumSeries(startvalue, endvalue));
