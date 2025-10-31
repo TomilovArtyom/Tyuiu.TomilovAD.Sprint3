594236b666fdacc468445ea6f40ba63811bfe515
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-31 21:49:47 +0500

    Task 0

--- Tyuiu.TomilovAD.Sprint3.Task0.V1.Lib/DataService.cs
+++ Tyuiu.TomilovAD.Sprint3.Task0.V1.Lib/DataService.cs
@@ -2,17 +2,20 @@
 
 namespace Tyuiu.TomilovAD.Sprint3.Task0.V1.Lib
 {
-    public class DataService : ISprint3Task0V3
+    public class DataService : ISprint3Task0V1
     {
-        public double GetSumSeries(int startValue, int stopValue)
+        public double GetSumSeries(int value, int startValue, int stopValue)
         {
-            double sumSeries = 0;
-            int i;
-            for (i = startValue; i <= stopValue; i++)
             {
-                sumSeries = sumSeries + (Math.Pow(4, 2) + i) + 1;
+                value = 4;
+                double sumSeries = 0;
+                int i;
+                for (i = startValue; i <= stopValue; i++)
+                {
+                    sumSeries = sumSeries + (Math.Pow(value, 2) + i) + 1;
+                }
+                return Math.Round(sumSeries, 3);
             }
-            return Math.Round(sumSeries, 3);
         }
     }
 }
--- Tyuiu.TomilovAD.Sprint3.Task0.V1.Test/DataServiceTest.cs
+++ Tyuiu.TomilovAD.Sprint3.Task0.V1.Test/DataServiceTest.cs
@@ -9,11 +9,11 @@ namespace Tyuiu.TomilovAD.Sprint3.Task0.V1.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-
+            int value = 4;
             int startvalue = 1;
             int endvalue = 15;
 
-            double res = ds.GetSumSeries(startvalue, endvalue);
+            double res = ds.GetSumSeries(value, startvalue, endvalue);
 
             double wait = 375;
 
